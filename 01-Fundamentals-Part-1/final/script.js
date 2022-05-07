e56ad5894fdcff8af253c3dabffaae74170fc384
frkylmz <farukyilmaz420@gmail.com>
2022-05-07 19:44:14 +0300

    first commit

---
+++ 01-Fundamentals-Part-1/final/script.js
@@ -0,0 +1,17 @@
+let js = "amazing";
+console.log(40+8+23-10);
+
+console.log("Faruk");
+console.log(23);
+
+let firstName = "Faruk";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "Faruk";
+let PI = 3.14159265;
