03e7e02a583d0ba6ade8300e63f4ac1d4a334212
frkylmz <farukyilmaz420@gmail.com>
2022-05-07 20:25:00 +0300

    add data-types-lesson

--- 01-Fundamentals-Part-1/final/script.js
+++ 01-Fundamentals-Part-1/final/script.js
@@ -1,3 +1,6 @@
+// Values and Variables
+
+/*
 let js = "amazing";
 console.log(40+8+23-10);
 
@@ -15,3 +18,58 @@ let $function = 27;
 
 let person = "Faruk";
 let PI = 3.14159265;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
+*/
+
+
+
+
+// Data Types
+
+// Primitive
+// Number
+    // let age = 23;
+
+// String
+    // let firstName = "Faruk"
+
+// Boolean (logical)
+    // let fullAge = true;
+    
+
+// Undefined
+    // let children;
+
+// Null (empty value)
+    // 
+
+// Symbol (ES2015)
+
+// BigInt (ES2020)
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true); //boolean
+console.log(typeof javascriptIsFun); //boolean
+console.log(typeof 23); //number
+console.log(typeof "Faruk"); //string
+
+javascriptIsFun = "YES"; 
+console.log(typeof javascriptIsFun); //string
+
+let year; // empty value 
+console.log(year); //undefined 
+console.log(typeof year); //undefined
+
+year = 1991;
+console.log(typeof year); //number
+
+console.log(typeof null); //object
---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,8 @@
+let country = "Turkey";
+console.log(country);
+
+let continent = "Europe";
+console.log(continent);
+
+let population = "84.680.273";
+console.log(population);
