89a76b5b27f1cf48b43e4d1bc7937032ba900141
frkylmz <farukyilmaz420@gmail.com>
2022-05-07 22:25:24 +0300

    add template-literals lesson

--- 01-Fundamentals-Part-1/final/script.js
+++ 01-Fundamentals-Part-1/final/script.js
@@ -143,6 +143,7 @@ console.log(now - 1995 > now - 1999);
 
 
 // Lesson 5 - Operator Precedence
+/*
 const now = 2022;
 const ageOmer = now - 1995;
 const ageFaruk = now - 1999;
@@ -157,8 +158,87 @@ console.log(x, y);
 
 const averageAge = (ageFaruk + ageOmer) / 2
 console.log(ageFaruk, ageOmer, averageAge);
+*/
 
 
 
 
 
+// Practice Assignments
+/*
+let country = "Turkey";
+console.log(country);
+
+let continent = "Europe";
+console.log(continent);
+
+let population = "84.680.273";
+console.log(population);
+*/
+
+
+
+
+
+// Coding Challenge
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+*/
+
+// Test Data 1
+// const massMark = 78;
+// const heightMark = 1.69;
+
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+
+
+// Test Data 2
+// const massMark = 95;
+// const heightMark = 1.99;
+
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+/*
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / ( heightJohn * heightJohn );
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
+*/
+
+
+
+
+
+// Lesson 6 - Strings and Template Literals
+
+const firstName = "Faruk";
+const job = "student";
+const birthYear = 1999;
+const year = 2037;
+
+const Faruk = "I'm " + firstName + ', a ' + (year - birthYear) + " year old " + job + "!";
+console.log(Faruk);
+
+const farukNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(farukNew);
+
+console.log(`Just a regular string...`);
+
+console.log("String with \n\
+multiple \n\
+lines");
+
+console.log(`String
+multiple
+lines`);
