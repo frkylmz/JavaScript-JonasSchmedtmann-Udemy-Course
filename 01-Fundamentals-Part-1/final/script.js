dc265b3e798b0f6ef407165f9250fde21cd04ca4
frkylmz <farukyilmaz420@gmail.com>
2022-05-09 19:43:43 +0300

    add truthy-falsy lesson

--- 01-Fundamentals-Part-1/final/script.js
+++ 01-Fundamentals-Part-1/final/script.js
@@ -341,3 +341,29 @@ GOOD LUCK 😀
 // let n = "1" + 1; // "11"
 // n = n - 1; // 11 = 11 - 1 ------> 10
 // console.log(n); // 10
+
+
+
+
+
+// Lesson 9 Truthy and Falsy Values
+// 5 falsy values : 0, "", undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Faruk"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 100;
+if(money) {
+    console.log("Dont' spend it all ;)");
+} else {
+    console.log("You should get a job!");
+}
+
+let height = 150;
+if(height) {
+    console.log("YAY! Height is defined");
+} else {
+    console.log("Height is UNDEFINED");
+}
