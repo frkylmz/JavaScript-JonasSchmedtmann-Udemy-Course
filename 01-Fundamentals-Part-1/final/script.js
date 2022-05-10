c5f246c2a633e021ff425bd0654f453d79733016
frkylmz <farukyilmaz420@gmail.com>
2022-05-10 18:08:28 +0300

    add switch-statement lesson

--- 01-Fundamentals-Part-1/final/script.js
+++ 01-Fundamentals-Part-1/final/script.js
@@ -493,3 +493,50 @@ GOOD LUCK 😀
 // } else {
 //     console.log("No one wins the trophy :(");
 // }
+
+
+
+
+
+// Lesson 13 switch statement
+
+const day = "friday";
+
+switch (day) {
+    case "monday": // day === "monday"
+        console.log("Plan course structure");
+        console.log("Go to coding meetup");
+        break;
+    case "tuesday":
+        console.log("Prepare theory videos");
+        break;
+    case "wednesday":
+    case "thursday":
+        console.log("Write code examples");
+        break;
+    case "friday":
+        console.log("Record videos");
+        break;
+    case "saturday":
+    case "sunday":
+        console.log("Enjoy the weekend :d");
+        break;
+    default:
+        console.log("Not a valid day!");   
+}
+
+if (day === "monday") {
+    console.log("Plan course structure");
+    onsole.log("Go to coding meetup");
+} else if (day === "tuesday") {
+    console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+    console.log("Write code examples");
+} else if (day === "friday") {
+    console.log("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+    console.log("Enjoy the weekend :d");
+} else {
+    console.log("Not a valid day!"); 
+}
+
