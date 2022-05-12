6ee7c4a880d385d809ce3c3534a9488c53b69825
frkylmz <farukyilmaz420@gmail.com>
2022-05-12 18:59:51 +0300

    add coding-challenge

--- 02-Fundamentals-Part-2/final/script.js
+++ 02-Fundamentals-Part-2/final/script.js
@@ -232,40 +232,75 @@ GOOD LUCK 😀
 // Lesson 7 Basic Array Operations (Methods)
 // Add elements
 // push - end of
-const friends = ["Michael", "Steven", "Peter"];
-const newLength = friends.push("Jay");  // adds elements to the end of an array 
-console.log(friends);
-console.log(newLength);
+// const friends = ["Michael", "Steven", "Peter"];
+// const newLength = friends.push("Jay");  // adds elements to the end of an array 
+// console.log(friends);
+// console.log(newLength);
 
-// unshift - beginning
-friends.unshift("John"); // add elements to the beginning of an array
-console.log(friends);
-
-
-// Remove elements
-// pop - last element
-friends.pop(); // Jay is gone
-const popped = friends.pop(); // Peter is gone
-console.log(popped);
-console.log(friends);
-
-// shift - first element
-friends.shift(); // John is gone
-console.log(friends); 
-
-console.log(friends.indexOf("Steven")); // index at which this element is located (1)
-console.log(friends.indexOf("Bob")); // -1
+// // unshift - beginning
+// friends.unshift("John"); // add elements to the beginning of an array
+// console.log(friends);
 
 
-// return true if the element is in the array and false if it's not
-friends.push(23);
-console.log(friends.includes("Steven")); // true
-console.log(friends.includes("Bob")); // false
-console.log(friends.includes("23")); // false (coercion) "23" -> string, 23 -> number
+// // Remove elements
+// // pop - last element
+// friends.pop(); // Jay is gone
+// const popped = friends.pop(); // Peter is gone
+// console.log(popped);
+// console.log(friends);
 
-if(friends.includes("Steven")) {
-    console.log("You have a friend called Steven.");
-}
+// // shift - first element
+// friends.shift(); // John is gone
+// console.log(friends); 
+
+// console.log(friends.indexOf("Steven")); // index at which this element is located (1)
+// console.log(friends.indexOf("Bob")); // -1
+
+
+// // return true if the element is in the array and false if it's not
+// friends.push(23);
+// console.log(friends.includes("Steven")); // true
+// console.log(friends.includes("Bob")); // false
+// console.log(friends.includes("23")); // false (coercion) "23" -> string, 23 -> number
+
+// if(friends.includes("Steven")) {
+//     console.log("You have a friend called Steven.");
+// }
+
+
+
+
+
+// Coding Challenge #2
+
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+
+HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+
+GOOD LUCK 😀
+*/
+
+// const calcTip = function (bill) {
+//     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// }
+
+// // const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+// console.log(bills, tips, totals);
+
+
 
 
 
