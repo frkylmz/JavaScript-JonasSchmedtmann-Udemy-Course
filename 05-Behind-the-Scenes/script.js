c56bd050fdd4e3cea822bec3685299cf9ca518bc
frkylmz <farukyilmaz420@gmail.com>
2022-05-20 18:29:16 +0300

    add Reguler Functions vs Arrow Functions lesson

--- 05-Behind-the-Scenes/script.js
+++ 05-Behind-the-Scenes/script.js
@@ -86,33 +86,82 @@
 // Lesson 3 The this Keyword
 // console.log(this);
 
-const calcAge = function (birthYear) {
-  console.log(2037 - birthYear);
-  // console.log(this);
-};
-calcAge(1999);
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   // console.log(this);
+// };
+// calcAge(1999);
 
-const calcAgeArrow = (birthYear) => {
-  console.log(2037 - birthYear);
-  // console.log(this);
-};
-calcAgeArrow(1980);
+// const calcAgeArrow = (birthYear) => {
+//   console.log(2037 - birthYear);
+//   // console.log(this);
+// };
+// calcAgeArrow(1980);
+
+// const faruk = {
+//   year: 1999,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year);
+//   },
+// };
+// faruk.calcAge();
+
+// const matilda = {
+//   year: 2017,
+// };
+
+// matilda.calcAge = faruk.calcAge;
+// matilda.calcAge();
+
+// const f = faruk.calcAge;
+// f();
+
+// Lesson 4 Regular Functions vs Arrow Functions
+
+// var firstName = "Matilda";
 
 const faruk = {
-  year: 1999,
+  firstName: "Faruk",
+  year: 1991,
   calcAge: function () {
-    console.log(this);
+    // console.log(this);
     console.log(2037 - this.year);
+
+    // Solution 1
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    // console.log(self.year >= 1981 && self.year <= 1996);
+    // console.log(this.year >= 1981 && this.year <= 1996);
+    // };
+
+    // Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
   },
 };
+faruk.greet();
 faruk.calcAge();
 
-const matilda = {
-  year: 2017,
+// arguments Keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
 };
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
 
-matilda.calcAge = faruk.calcAge;
-matilda.calcAge();
-
-const f = faruk.calcAge;
-f();
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
