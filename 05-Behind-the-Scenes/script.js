e4cb88eaee86465b8c39720afbb267b93800aec1
frkylmz <farukyilmaz420@gmail.com>
2022-05-22 13:44:42 +0300

    add Primitives vs Objects lesson

--- 05-Behind-the-Scenes/script.js
+++ 05-Behind-the-Scenes/script.js
@@ -217,4 +217,4 @@
 // jessicaCopy.family.push("John");
 
 // console.log("Before marriage:", jessica2);
-// console.log("After marriage:", jessicaCopy);
+console.log("After marriage:", jessicaCopy);
